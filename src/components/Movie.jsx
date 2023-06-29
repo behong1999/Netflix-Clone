86359006ad801279faad517b257e05c3532ba6ef
behong1999 <khonhong46632@gmail.com>
2023-06-29 12:28:04 +0300

    fix

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { UserAuth } from '../context/AuthContext';
